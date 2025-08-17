99f31dfda5b5bbc03baa3f0e023bc1c8366336fb
Logan Gerber <logangerber@protonmail.com>
2025-08-17 16:24:15 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+	return (
+		<div id="app">
+			<div id="leftbar">
+
+			</div>
+			<div id="leftimgheader">
+
+			</div>
+			<div id="rightimgheader">
+
+			</div>
+			<div id="leftimg">
+
+			</div>
+			<div id="rightimg">
+
+			</div>
+			<div id="keepleft">
+
+			</div>
+			<div id="skip">
+
+			</div>
+			<div id="keepright">
+
+			</div>
+		</div>
+	);
+}
+
+export default App;
